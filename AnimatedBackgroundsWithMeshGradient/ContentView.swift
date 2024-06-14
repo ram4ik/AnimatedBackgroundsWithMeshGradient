e2499ab850a37f55b152a242a9561b3a9a335bf6
ram4ik <ram4ik@hotmail.com>
2024-06-14 19:43:42 +0300

    init

--- AnimatedBackgroundsWithMeshGradient/ContentView.swift
+++ AnimatedBackgroundsWithMeshGradient/ContentView.swift
@@ -19,6 +19,31 @@ struct ContentView: View {
     }
 }
 
+struct MeshGadientView: View {
+    @State private var isAnimating = false
+    
+    let colors1 = (0..<9).map { _ in [Color.green, .blue, .cyan].randomElement()! }
+    let colors2 = (0..<9).map { _ in [Color.green, .blue, .cyan].randomElement()! }
+    
+    var body: some View {
+        MeshGradient(
+            width: 3,
+            height: 3,
+            points: [
+                .init(x: 0, y: 0), .init(x: 0.5, y: 0), .init(x: 1, y: 0),
+                .init(x: 0, y: 0.5), .init(x: 0.5, y: 0.5), .init(x: 1, y: 0.5),
+                .init(x: 0, y: 1), .init(x: 0.5, y: 1), .init(x: 1, y: 1),
+            ],
+            colors: isAnimating ? colors1 : colors2
+        )
+        .onAppear {
+            withAnimation(.easeInOut(duration: 5).repeatForever()) {
+                isAnimating.toggle()
+            }
+        }
+    }
+}
+
 #Preview {
     ContentView()
 }
