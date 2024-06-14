9b533edec157891cefd40bb61d84f049dbdcc05e
ram4ik <ram4ik@hotmail.com>
2024-06-14 19:32:06 +0300

    Initial Commit

---
+++ AnimatedBackgroundsWithMeshGradient/AnimatedBackgroundsWithMeshGradientApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedBackgroundsWithMeshGradientApp.swift
+//  AnimatedBackgroundsWithMeshGradient
+//
+//  Created by Ramill Ibragimov on 14.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedBackgroundsWithMeshGradientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnimatedBackgroundsWithMeshGradientTests/AnimatedBackgroundsWithMeshGradientTests.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedBackgroundsWithMeshGradientTests.swift
+//  AnimatedBackgroundsWithMeshGradientTests
+//
+//  Created by Ramill Ibragimov on 14.06.2024.
+//
+
+import Testing
+@testable import AnimatedBackgroundsWithMeshGradient
+
+struct AnimatedBackgroundsWithMeshGradientTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
